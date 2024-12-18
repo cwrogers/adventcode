136caaf121e8ac2ebc4bddc11622f09d92dcfecb
crogers <cwrogiv@gmail.com>
2024-12-17 23:58:16 -0700

    7 was way ezier than 6

---
+++ python/aoc/07/2.py
@@ -0,0 +1,27 @@
+#! /usr/bin/python3
+import itertools
+
+with open("input.txt", "r") as file:
+    lines = file.read().strip().split('\n')
+
+ops = ['+', '*', '|']
+sum = 0
+for line in lines:
+    l = line.split(':')
+    target = int(l[0])
+    nums = list(map(lambda x: int(x),l[1].strip().split(' ')))
+    for opl in list(itertools.product(ops, repeat=len(nums) - 1)):
+        t = nums[0]
+        for n in range(1,len(nums)):
+            if opl[n-1] == '+':
+                t += nums[n]
+            elif opl[n-1] == '*':
+                t *= nums[n]
+            elif opl[n-1] == '|':
+                t = int(str(t) + str(nums[n]))
+        if t == target:
+            sum += target
+            break
+
+
+print(sum)
